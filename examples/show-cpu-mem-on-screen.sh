b891189794efdd4ddfe42f292a9e7253bdd6e56f
kphf1995cm <kphf1995cm@outlook.com>
2018-04-10 19:25:38 +0800

    NS4 Version 3 (Remove Ns3PacketAndPort Struct, Fix memory leak, Add ns3-large-scale.cc ns4-large-scale.cc srt-fattree.cc experiment, Fix port parameter passing bug, Split P4Model and P4NetDevice)

--- examples/show-cpu-mem-on-screen.sh
+++ examples/show-cpu-mem-on-screen.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-while test 1=1;do ps -aux | grep ns3.26-p4-examp | grep -v grep | awk '{print "cpu: "$3" mem:"$4}';sleep 1;done 
+while test 1=1;do ps -aux | grep ns3.26-p4-examp | grep -v grep | awk '{print "cpu: "$3" mem:"$4}';sleep 5;done 
 
 
